4293f2d672c9d95e1ec17874d0832183091f9dda
Sandip <sandi.bag3@gmail.com>
2026-05-17 02:27:55 +0530

    Configure Firebase for web and desktop

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,13 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'auth_gate.dart';
 import 'app_state.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   final appState = AppState();
   await appState.loadProfile();
